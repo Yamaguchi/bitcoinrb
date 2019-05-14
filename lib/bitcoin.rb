fde1783bc4f0c1e7aab0ff6214481a3d64076d65
mike <mike@chaintope.com>
2019-05-14 13:14:10 +0800

    add utxo management

--- lib/bitcoin.rb
+++ lib/bitcoin.rb
@@ -56,6 +56,7 @@ module Bitcoin
   autoload :SLIP39, 'bitcoin/slip39'
   autoload :Aezeed, 'bitcoin/aezeed'
   autoload :PaymentCode, 'bitcoin/payment_code'
+  autoload :Utxo, 'bitcoin/utxo'
 
   require_relative 'bitcoin/constants'
 
